810c9718c96542d67955594febe1e1d9e667dbb7
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-05 13:25:54 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c=1;
+  int d=1;
+  printf("\nEnter a number");
+  scanf("%d",&b);
+  printf("\nEnter power");
+  scanf("%d",&a);
+  while(c<=a)
+  {
+  d=d*b;
+  c++;
+  }
+  printf("\n%d",d);
+  return 0;
+}
